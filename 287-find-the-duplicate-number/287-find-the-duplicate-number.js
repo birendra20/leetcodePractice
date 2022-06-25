db0162a25775133d6f8537aee1cdd3011dcafdb2
birendra gupta <96100549+birendra20@users.noreply.github.com>
2022-06-25 10:54:40 +0530

    Time: 10126 ms (5.01%), Space: 49.8 MB (77.14%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+    for (let i = 0; i < nums.length-1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                return nums[i]
+            }
+        }
+    }
+};
+
+// var findDuplicate = function(nums) {
+   
+    
+// };
+
+
+
+
+
+
