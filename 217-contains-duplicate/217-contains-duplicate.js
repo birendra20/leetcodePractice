be20f4943876352a802f2cdf9c7cd266c74d0007
birendra gupta <96100549+birendra20@users.noreply.github.com>
2022-06-26 10:23:59 +0530

    Time: 92 ms (90.20%), Space: 50 MB (86.92%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,57 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+    
+    let set =new Set();
+    
+    for(let i=0;i<nums.length;i++){
+      
+        if(set.has(nums[i])){
+            return true;
+        }else{
+            
+            set.add(nums[i])
+        }
+    
+    }
+    
+    return false
+    
+    
+    
+    
+    
+//     let obj={};
+    
+//     for(let i=0;i<nums.length;i++){
+        
+        
+//         if(obj[nums[i]]==undefined){
+//             obj[nums[i]]=1
+//         }else{
+//             obj[nums[i]]++
+//         }
+//     }
+    
+    
+//     let count=0;
+//     let maxcount=0
+//     for(let key in obj){
+//         if(obj[key]<=1){
+//             return false
+//         }else{
+//             return true
+//         }
+    
+    
+    
+    
+   // }
+    
+        
+        
+        
+};
