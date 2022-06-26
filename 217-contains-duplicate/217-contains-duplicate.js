0f53060091a39c89e194e7152dc30f0b0fe8b2f1
birendra gupta <96100549+birendra20@users.noreply.github.com>
2022-06-26 10:27:05 +0530

    Time: 310 ms (16.18%), Space: 54.7 MB (5.48%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -5,53 +5,50 @@
 var containsDuplicate = function(nums) {
     
     
-    let set =new Set();
-    
-    for(let i=0;i<nums.length;i++){
-      
-        if(set.has(nums[i])){
-            return true;
-        }else{
-            
-            set.add(nums[i])
-        }
-    
-    }
-    
-    return false
-    
-    
-    
-    
-    
-//     let obj={};
+//     let set =new Set();
     
 //     for(let i=0;i<nums.length;i++){
-        
-        
-//         if(obj[nums[i]]==undefined){
-//             obj[nums[i]]=1
+      
+//         if(set.has(nums[i])){
+//             return true;
 //         }else{
-//             obj[nums[i]]++
+            
+//             set.add(nums[i])
 //         }
+    
 //     }
     
+//     return false
     
-//     let count=0;
-//     let maxcount=0
-//     for(let key in obj){
-//         if(obj[key]<=1){
-//             return false
-//         }else{
-//             return true
-//         }
+// }
     
     
     
+    let obj={};
     
-   // }
-    
+    for(let i=0;i<nums.length;i++){
         
         
+        if(obj[nums[i]]==undefined){
+            obj[nums[i]]=1
+        }else{
+            obj[nums[i]]++
+        }
+    }
+    
+    
+    // let count=0;
+    // let maxcount=0
+    for(let key in obj){
+        if(obj[key]>1){
+            return true
+
+    
+    
+    
+   }
+    
+    }      
+    return false    
         
 };
