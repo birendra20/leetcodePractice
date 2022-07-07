28290c546c2dfc9855489f56f086a7af5b5c96c2
birendra gupta <96100549+birendra20@users.noreply.github.com>
2022-07-07 10:09:00 +0530

    Time: 83 ms (94.91%), Space: 45.1 MB (34.78%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -4,24 +4,24 @@
 //loop through a second time, setting items to 0, if their indexes are in either set
 
 const setZeroes = (matrix) => {
-    let rowsZero =Array(matrix.length).fill(null)
-    let colsZero = Array(matrix[0].length).fill(null)
+    let dummyRow =Array(matrix.length).fill(null)
+    let dummyCol = Array(matrix[0].length).fill(null)
     for (let i = 0;i < matrix.length;i++){
         for (let j = 0;j<matrix[0].length;j++){
             if (matrix[i][j] === 0){
-                // rowsZero.add(i)
-                // colsZero.add(j)
-                rowsZero[i]=0;
-                colsZero[j]=0
+               
+                dummyRow[i]=0;
+                dummyCol[j]=0
             }
         }
     }
   for (let i = 0;i < matrix.length;i++){
         for (let j = 0;j<matrix[0].length;j++){
-            if ( rowsZero[i]==0 ||  colsZero[j]==0){
+            if ( dummyRow[i]==0 ||  dummyCol[j]==0){
                 matrix[i][j] = 0
             }
         }
     }
+    return matrix
 
 }
